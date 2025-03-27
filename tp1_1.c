ec0de8591782d2f34f0fe3bfc7a3133eb33c2d09
NicoMore02 <nicomore053@gmail.com>
2025-03-26 22:26:03 -0300

    segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <conio.h>
 
 int main() {
-    printf ("Hola Mundo");
+    printf ("Hola Mundo\n");
+
+    int numero = 5;
+    int *puntero = &numero;
+
+    printf("contenido del puntero: %d\n", *puntero);
+    printf("direccion de memoria almacenada por el puntero: %p\n", (void*)puntero);
+    printf("direccion de memoria de la variable: %p\n", (void*)&numero);
+    printf("direccion de memoria del puntero: %p\n", (void*)&puntero);
+    printf("el tamano de la memoria es: %d\n", sizeof(numero));
+    
+
+    getch();
+
+    return 0;
 }
